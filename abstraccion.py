ebc9a37c3728f33d3846ea195d23b04dc53fa7f2
angelDariaux <angeldariaux@gmail.com>
2022-07-18 21:43:12 -0600

    concepto de abstracciones

---
+++ abstraccion.py
@@ -0,0 +1,29 @@
+# La abtraccion es que nosotros solo lidiamos con lo que nos interesa no con los pequenos detalles de como funciona algo por dentro
+
+class Lavadora:
+
+  def __init__(self):
+    pass
+
+  def lavar(self, temperatura='caliente'):
+    self._llenar_tanque_de_agua(temperatura)
+    self._anadir_jabon()
+    self._lavar()
+    self._centrifugar()
+
+  def _llenar_tanque_de_agua(self, temperatura):
+    print(f'Llenando el tanque con agua {temperatura}')
+
+  def _anadir_jabon(self):
+    print('Anadiendo jabon')
+
+  def _lavar(self):
+    print('Lavando la ropa')
+
+  def _centrifugar(self):
+    print('Centrrifugando la ropa')
+
+
+if __name__ == "__main__":
+  lavadora = Lavadora()
+  lavadora.lavar()
